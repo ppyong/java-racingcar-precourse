d6f15a1e9c900fdd74cc56650cb48ee8cd6de7a7
Tony <jian11@coupang.com>
2021-05-02 22:36:45 +0900

    feat: 랜덤 기능 구현
    - 0-9 사이 랜덤 정수를 구하는 class 구현
    
    Break ${browser}

--- src/main/java/nextstep/utils/RandomGenerator.java
+++ src/main/java/nextstep/utils/RandomGenerator.java
@@ -1,8 +1,14 @@
 package nextstep.utils;
 
+import java.util.Random;
+
 public class RandomGenerator {
 
-    public int create() {
-        return -1;
+    private final static int MIN = 0;
+    private final static int MAX = 9;
+    private final static Random random = new Random();
+
+    public static int create() {
+        return random.nextInt(MAX + MIN) + MIN;
     }
 }
